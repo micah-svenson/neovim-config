37d4b4ddb322b00a6b1e1b5bb6ab11dfa8b32a42
micah-svenson <38704799+micah-svenson@users.noreply.github.com>
2023-02-12 20:53:26 -0700

    feat: add descriptions to keymaps in remap.lua

--- lua/micah/remap.lua
+++ lua/micah/remap.lua
@@ -1,34 +1,33 @@
 vim.g.mapleader = " "
 
 -- clear highlighted search with ctrl+l
-vim.keymap.set({"i", "v", "n"}, "<C-l>", ":nohl<CR><C-l>", {  })
+vim.keymap.set({"i", "v", "n"}, "<C-l>", ":nohl<CR><C-l>", { desc = "Clear highlights" })
 
--- escape crutch
-vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
+vim.keymap.set("i", "kj", "<Esc>", { noremap = true, desc = "Ahh, much better way to escape insert mode"})
 
 -- project view (aka netrw)
 --  For vert sidebar split, set to vim.cmd.Vexplore
 --  For whole window, set to vim.cmd.Ex
-vim.keymap.set("n", "<leader>pv", vim.cmd.Lexplore)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Lexplore, { desc = "[P]roject [V]iew opens netrw" })
 
 -- "Drag" selected lines up and down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Drag selection down one line" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Drag selection up one line" })
 
 
 -- Keep cursor in place as you are appending rows
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Keep cursor in place when appending rows" })
 
 -- Keep cursor in the middle of the screen when scrolling with d, u
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Keep cursor in the middle of the screen when scrolling down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Keep cursor in the middle of the screen when scrolling up" })
 
 -- keep search terms in the middle of the screen
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv", { desc = "Keep next search term in the middle of the screen" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Keep previous search terms in the middle of the screen" })
 
 -- yanks into a secondary buffer so you dont lose your yank
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Yank into an alternate buffer so it doesnt get lost" })
 
 -- next greatest remap ever : asbjornHaland
 -- TODO: what does this do
