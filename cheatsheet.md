9e7220e8b572bc84d7ece6022008d9ba845c7774
micah svenson <micah.svenson@bluestaq.com>
2025-03-24 16:09:15 -0600

    fix: add wordwrap to picker preview and results list

---
+++ cheatsheet.md
@@ -0,0 +1,5 @@
+## Cheat sheet and Knowledge base
+
+**Focus preview window in file picker**
+
+- Alt+w
---
+++ lua/plugins/snacks-picker.lua
@@ -0,0 +1,21 @@
+return {
+  "snacks.nvim",
+  opts = {
+    picker = {
+      win = {
+        preview = {
+          wo = {
+            -- wrap text in the preview window
+            wrap = true,
+          },
+        },
+        list = {
+          wo = {
+            -- wrap text in the list results window
+            wrap = true,
+          },
+        },
+      },
+    },
+  },
+}
