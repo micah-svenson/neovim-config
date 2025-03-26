2919e3c37af73e8437a264c5f968c916c555f43b
micah svenson <micah.svenson@bluestaq.com>
2025-03-26 08:17:39 -0600

    fix: show hidden and ignored files by default in explorer

---
+++ lua/plugins/explorer.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          -- show hidden files by default
+          hidden = true,
+          -- show ignored files be default
+          ignored = true,
+        },
+      },
+    },
+  },
+}
